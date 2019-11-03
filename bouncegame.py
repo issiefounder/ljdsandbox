5117b8aaf29a18a2635be5db4170cab3a14b39f9
maeldun-x <m@mdoyle.com>
2019-11-03 07:59:58 -0800

    Clean up

--- bouncegame.py
+++ bouncegame.py
@@ -38,14 +38,13 @@ class Paddle:
 
     def __init__(self, canvas, color):
         self.canvas = canvas
-        self.id = canvas.create_rectangle(0, 0, 100, 10, fill = color)
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
+        # self.canvas.focus_set()
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
-
-
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -54,7 +53,6 @@ class Paddle:
             self.x = 0
         elif pos[2] >= self.canvas_width:
             self.x = 0
-
 
 
 tk = Tk()
